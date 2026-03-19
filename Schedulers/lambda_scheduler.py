9fa29c86db1ce7979156dd872bee7322879191ca
NathanRoland <nathanroland635@gmail.com>
2026-03-19 19:05:23 +1100

    fix: modified test steps for lower time

--- Schedulers/lambda_scheduler.py
+++ Schedulers/lambda_scheduler.py
@@ -17,7 +17,16 @@ class LambdaLR(LRScheduler):
         self.lr_lambda = lr_lambda
         super().__init__(optimizer, last_epoch)
 
+    def state_dict(self):
+        return {k: v for k, v in self.__dict__.items()
+                if k not in ("optimizer", "lr_lambda")}
+
+    def load_state_dict(self, state_dict):
+        lr_lambda = self.lr_lambda
+        self.__dict__.update(state_dict)
+        self.lr_lambda = lr_lambda
+
     def get_lr(self):
         t = self.last_epoch
         factor = self.lr_lambda(t)
-        return [base_lr + factor for base_lr in self.base_lrs]
+        return [base_lr * factor for base_lr in self.base_lrs]
